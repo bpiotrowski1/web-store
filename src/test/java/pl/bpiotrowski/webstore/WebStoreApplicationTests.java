c676cdf7eb6284b2c126a2351785d4f8b6a7708a
Błażej Piotrowski <baej.p26@gmail.com>
2019-10-27 14:40:38 +0100

    Initial commit

---
+++ src/test/java/pl/bpiotrowski/webstore/WebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bpiotrowski.webstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
