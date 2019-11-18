3b289325b10082fd415ff8d555a6f34cc1c3b2b2
bpiotrowski <baej.p26@gmail.com>
2019-11-18 15:15:28 +0100

    Add billing address to order details

--- src/main/java/pl/bpiotrowski/webstore/service/UserService.java
+++ src/main/java/pl/bpiotrowski/webstore/service/UserService.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.bpiotrowski.webstore.dto.UserDto;
+import pl.bpiotrowski.webstore.entity.OrderHeader;
 import pl.bpiotrowski.webstore.entity.Role;
 import pl.bpiotrowski.webstore.entity.User;
+import pl.bpiotrowski.webstore.exception.EntityNotFoundException;
 import pl.bpiotrowski.webstore.exception.PasswordNotMatchException;
+import pl.bpiotrowski.webstore.repository.OrderHeaderRepository;
 import pl.bpiotrowski.webstore.repository.RoleRepository;
 import pl.bpiotrowski.webstore.repository.UserRepository;
 
@@ -17,6 +20,22 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final OrderHeaderRepository orderHeaderRepository;
+
+    public UserDto findAddressByOrderId(Long id) {
+        UserDto dto = new UserDto();
+        OrderHeader orderHeader = orderHeaderRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Order header " + id + " not found"));
+        User entity = userRepository.findById(orderHeader.getPurchaser().getId())
+                .orElseThrow(() -> new EntityNotFoundException("User " + id + " not found"));
+
+        dto.setFirstName(entity.getFirstName());
+        dto.setLastName(entity.getLastName());
+        dto.setStreet(entity.getStreet());
+        dto.setHouseNumber(entity.getHouseNumber());
+
+        return dto;
+    }
 
     public void create(UserDto userForm) {
         if(!userForm.getPassword().equals(userForm.getRepeatPassword())) {
