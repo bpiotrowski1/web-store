1e7703d5d31c9ad47caebb3d5c8447d899d8cff7
bpiotrowski <baej.p26@gmail.com>
2020-01-29 14:54:27 +0100

    Forbidden exception

--- src/main/java/pl/bpiotrowski/webstore/handler/WebstoreExceptionHandler.java
+++ src/main/java/pl/bpiotrowski/webstore/handler/WebstoreExceptionHandler.java
@@ -1,17 +1,18 @@
 package pl.bpiotrowski.webstore.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import pl.bpiotrowski.webstore.exception.QuantityBelowZeroException;
 
 @Slf4j
 @RestControllerAdvice
-public class MyExceptionHandler {
+public class WebstoreExceptionHandler {
 
     @ExceptionHandler(QuantityBelowZeroException.class)
     public String quantityBelowZeroException(QuantityBelowZeroException e) {
-        log.error("QUANTITY BELOW ZERO");
+        log.error(e.getMessage());
         return e.getMessage();
     }
 
