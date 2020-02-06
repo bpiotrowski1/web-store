c5913042c0608c0700286b1fec05790bf24b5952
bpiotrowski <baej.p26@gmail.com>
2020-02-06 13:58:09 +0100

    Added thumbnail controller, service

--- src/main/java/pl/bpiotrowski/webstore/service/ThumbnailService.java
+++ src/main/java/pl/bpiotrowski/webstore/service/ThumbnailService.java
@@ -1,24 +1,35 @@
 package pl.bpiotrowski.webstore.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+@RequiredArgsConstructor
 @Service
 public class ThumbnailService {
 
-    public void save(MultipartFile thumbnail) {
+    private final ProductService productService;
+
+    public void save(Long id, MultipartFile thumbnail) {
         try {
             byte[] bytes = thumbnail.getBytes();
-            File file = new File("test.jpg");
-            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(file));
-            stream.write(bytes);
-            stream.close();
+            Path path = Paths.get(thumbnail.getOriginalFilename());
+            Files.write(path, bytes);
+            productService.changeThumbnail(id, path.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        catch (Exception e) {
-            return;
-        }
+    }
+
+    public Resource findThumbnail(Long id) {
+        return new FileSystemResource(productService.getOne(id).getThumbnail());
     }
 
 }
