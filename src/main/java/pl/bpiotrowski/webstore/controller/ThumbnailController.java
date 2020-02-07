a406ea45792ecb64c718605d5816a9a4b279c349
bpiotrowski <baej.p26@gmail.com>
2020-02-07 10:21:54 +0100

    Change display thumbnails for users

--- src/main/java/pl/bpiotrowski/webstore/controller/ThumbnailController.java
+++ src/main/java/pl/bpiotrowski/webstore/controller/ThumbnailController.java
@@ -10,23 +10,22 @@ import pl.bpiotrowski.webstore.service.ThumbnailService;
 
 @RequiredArgsConstructor
 @Controller
-@RequestMapping("/admin/thumbnail")
 public class ThumbnailController {
 
     private final ThumbnailService thumbnailService;
 
-    @GetMapping
+    @GetMapping("/admin/thumbnail")
     public String getFrom() {
         return "admin/thumbnail";
     }
 
-    @GetMapping(value = "/{productId}", produces = MediaType.IMAGE_JPEG_VALUE)
+    @GetMapping(value = "/thumbnail/{productId}", produces = MediaType.IMAGE_JPEG_VALUE)
     @ResponseBody
     public Resource getThumbnail(@PathVariable(name = "productId") Long productId) {
         return thumbnailService.findThumbnail(productId);
     }
 
-    @PostMapping
+    @PostMapping("/admin/thumbnail")
     public String uploadThumbnail(@RequestParam("id") Long id, @RequestParam("thumbnail") MultipartFile thumbnail) {
         thumbnailService.save(id, thumbnail);
         return "redirect:/admin/products";
